aa70f9fb8c99ca82bbf95709932eec202ff2f5ba
Gramma <gramma1997ion@gmail.com>
2022-11-26 15:01:01 +0200

    created method CreateStringArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArray()
+{
+    Console.WriteLine("Tape amount of strings");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+        {
+        Console.WriteLine($"Tape string №{i+1}");
+        array[i] = Console.ReadLine();
+        }
+    return array;
+}
